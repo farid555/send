6014253c7f4a3dd7ea1944669cb9feeacb69e512
farid555 <mohammad.uzzaman2@centria.fi>
2021-01-19 17:46:04 +0200

    addded signup models...

--- signupfront/signupbackend/routes/routes.js
+++ signupfront/signupbackend/routes/routes.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const router = express.Router()
+const singupTemplateCopy = require('../models/SigUpModels')
+const { request } = require('express')
 
 
 
 router.post('/signup', (req, res) => {
-    res.send(send)
+    const signedUpUser = new singupTemplateCopy({
+
+        fullName: request.body.fullName,
+        username: request.body.username,
+        email: request.body.email,
+        password: request.body.password
+    })
+    signUpUser.save()
+        .then(data => {
+            res.json(data)
+        })
+        .catch(error => {
+            res.json(error)
+        })
 })
 
-module.exports = router
+module.exports = router; 
--- signupfront/signupbackend/server.js
+++ signupfront/signupbackend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
+const
 
 
 
-app.listen(4000, () => console.log("Server is running..."))
+    app.listen(4000, () => console.log("Server is running..."))
