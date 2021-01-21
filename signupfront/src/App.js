cda1f183958cac9d5c6f50ee7f9ebf27be162ffc
farid555 <mohammad.uzzaman2@centria.fi>
2021-01-21 09:59:49 +0200

    outlook

--- signupfront/src/App.js
+++ signupfront/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react' //class based component
 import "bootstrap/dist/css/bootstrap.min.css"
 import axios from 'axios'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 class App extends Component {
     constructor() {
@@ -68,43 +71,51 @@ class App extends Component {
         return (
 
             <div>
-                <div className='container'>
-                    <div className='form-div'>
 
-                        <form onSubmit={this.onSubmit}>
+                <Container>
+                    <Header />
+                </Container>
+                <main className='py-3'>
+                    <div className='container'>
+                        <div className='form-div'>
 
-                            <input type='text'
-                                placeholder='Full Name'
-                                onChange={this.changeFullName}
-                                value={this.state.fullName}
-                                className='form-control form-group'
-                            />
+                            <form onSubmit={this.onSubmit}>
 
-                            <input type='text'
-                                placeholder='Username'
-                                onChange={this.changeUsername}
-                                value={this.state.username}
-                                className='form-control form-group'
-                            />
+                                <input type='text'
+                                    placeholder='Full Name'
+                                    onChange={this.changeFullName}
+                                    value={this.state.fullName}
+                                    className='form-control form-group'
+                                />
 
-                            <input type='text'
-                                placeholder='E-mail'
-                                onChange={this.changeEmail}
-                                value={this.state.email}
-                                className='form-control form-group'
-                            />
-                            <input type='password'
-                                placeholder='Possword'
-                                onChange={this.changePassword}
-                                value={this.state.password}
-                                className='form-control form-group'
-                            />
+                                <input type='text'
+                                    placeholder='Username'
+                                    onChange={this.changeUsername}
+                                    value={this.state.username}
+                                    className='form-control form-group'
+                                />
 
-                            <input type="submit" className="btn btn-danger btn-block" value='Submit' />
-                        </form>
+                                <input type='text'
+                                    placeholder='E-mail'
+                                    onChange={this.changeEmail}
+                                    value={this.state.email}
+                                    className='form-control form-group'
+                                />
+                                <input type='password'
+                                    placeholder='Possword'
+                                    onChange={this.changePassword}
+                                    value={this.state.password}
+                                    className='form-control form-group'
+                                />
 
+                                <input type="submit" className="btn btn-danger btn-block" value='Submit' />
+                            </form>
+                            <footer>
+                                <Footer />
+                            </footer>
+                        </div  >
                     </div>
-                </div>
+                </main>
             </div>
         )
     }
---
+++ signupfront/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+
+const Header = () => {
+    return (
+        <header>
+            <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
+                <Container>
+                    <Navbar.Brand href="#/">Signup Apps</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                            <Nav.Link href="#/logout">Sinout</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    )
+}
+
+export default Header
